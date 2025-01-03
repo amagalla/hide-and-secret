07ce450a68ac39099d150f30445444e80ca5e039
Anthony <anthonymagallanes24@gmail.com>
2025-01-03 21:40:12 +0800

    feat(controller/profiles, controller/registration, server/package, api/profiles, types/profiles): added login route to profiles, refactored register route, added controller logic to login route

--- app/server/routes/api/profiles.ts
+++ app/server/routes/api/profiles.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
 import createError from 'http-errors';
-import { registerUser } from '../../controller/registration';
-import { RegisterUserResponse } from '../../types/profiles.types';
+import { registerUser, loginUser } from '../../controller/profiles';
+import { RegisterUserResponse, LoginUserResponse } from '../../types/profiles.types';
 import hashPassword from '../../middleware/profiles/hash-password';
 import emailValidator from '../../middleware/profiles/email-validator';
 import usernameValidator from '../../middleware/profiles/username-validator';
@@ -22,6 +22,16 @@ const router = express.Router();
  *              username:
  *                  type: string
  *                  example: amagalla
+ *              password:
+ *                  type: string
+ *                  example: abcd1234
+*      LoginUser:
+ *          type: object
+ *          description: User login
+ *          properties:
+ *              email:
+ *                  type: string
+ *                  example: email@email.test
  *              password:
  *                  type: string
  *                  example: abcd1234
@@ -55,26 +65,79 @@ router.post(
     emailValidator,
     usernameValidator,
     hashPassword,
-    async (req: Request, res: Response, next: NextFunction) => {
-        let resp: RegisterUserResponse | undefined;
+    async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+        let resp: RegisterUserResponse;
 
         const { email, username, password } = req.body;
 
         try {
             resp = await registerUser(email, username, password);
+            
+            if (resp && resp.error) {
+                const status = resp.status || 500;
+                return next(createError(status, `${resp.error}`));
+            }
+            
+            res.status(200).send(resp)
+        } catch (err: unknown) {
+            if (err instanceof Error) {
+                return next(createError(`${err.message}` || 'An error has occured'));
+            }
+        }
+    }
+);
+
+/**
+ * @swagger
+ * 
+ *  /api/profiles/login:
+ * 
+ *  post:
+ *      description: Verifies user login credentials
+ *      produces:
+ *          - application/json
+ *      parameters:
+ *          - in: body
+ *            name: Login
+ *            description: User profile information
+ *            required: true
+ *            schema:
+ *              $ref: '#/definitions/LoginUser'
+ *      responses:
+ *          200:
+ *              description: Loging successful
+ *          400:
+ *              description: Login failed
+ */
+
+router.post(
+    '/login',
+    emailValidator,
+    async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+        let resp: LoginUserResponse | undefined;
+
+        const { email, password } = req.body;
+
+        if (!password) {
+            return next(createError(400, 'Password required'));
+        }
+
+        try {
+            resp = await loginUser(email, password);
+            
+            if (resp && resp.error) {
+                const status = resp.status || 500;
+                return next(createError(status, `${resp.error}`));
+            }
+    
+            res.status(200).send(resp);
         } catch (err: unknown) {
             if (err instanceof Error) {
                 return next(createError(`${err.message}` || 'An error has occured'));
             }
         }
 
-        if (resp && resp.error) {
-            const status = resp.status || 500;
-            return next(createError(status, `${resp.error}`));
-        }
-        
-        res.status(200).send(resp)
     }
-)
+);
 
 export default router;
