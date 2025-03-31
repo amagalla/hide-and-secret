0d328a7b5172c25bf20eb713d68d1b95b6a1b8ee
Anthony <anthonymagallanes24@gmail.com>
2025-03-31 12:16:32 -0700

    feat(migrations/public_secrets, migrations/secret_stash): added public secret and secret stash tables

---
+++ app/server/db/knex/migrations/20250331190534_secret_stash.ts
@@ -0,0 +1,20 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.raw(`
+        CREATE TABLE IF NOT EXISTS secret_stash (
+            message_id      BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+            message         VARCHAR(500),
+            id              BIGINT UNSIGNED NOT NULL,
+            PRIMARY KEY     (message_id),
+            FOREIGN KEY     (id) REFERENCES profiles(id) ON DELETE CASCADE
+        );
+    `)
+}
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.raw(`
+        drop table if exists secret_stash;
+    `)
+}
+
