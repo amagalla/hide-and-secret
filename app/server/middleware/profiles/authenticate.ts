b76bce5bd5506f40c9a8b2cd62d3d6049e884fd3
Anthony <anthonymagallanes24@gmail.com>
2025-03-31 19:59:55 -0700

    fix(profiles/authenticate): changed status code and fixed spelling for denial of access

--- app/server/middleware/profiles/authenticate.ts
+++ app/server/middleware/profiles/authenticate.ts
@@ -19,12 +19,13 @@ const authenticateToken = (req: authUserInfo, res: Response, next: NextFunction)
     const token = req.header('Authorization')?.replace('Bearer ', '');
 
     if (!token) {
-        return next(createError(400, "Acess denided. No token provided."));
+        return next(createError(401, "Access denied. No token provided."));
     }
 
     try {
         const decoded = jwt.verify(token, JWT_SECRET);
         req.user = decoded as UserPlayload;
+
         next();
     } catch (err) {
         return next(createError(400, "Invalid token."));
